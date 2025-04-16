f5f9197d38cb440728315b62dbca7e08cff06615
KenanGambarov <kenan.gambarov@gmail.com>
2025-04-16 09:08:33 +0400

    InternalProductController added

---
+++ src/main/java/com/productservice/controller/InternalProductController.java
@@ -0,0 +1,22 @@
+package com.productservice.controller;
+
+import com.productservice.dto.response.ProductResponseDto;
+import com.productservice.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("internal/v1/products")
+public class InternalProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductResponseDto getProductById(@PathVariable("id") Long id){
+        return productService.getProductById(id);
+    }
+
+}
